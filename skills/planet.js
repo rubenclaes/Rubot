87ec3f1227e4d5d083fba8671f25450b88204266
Ruben Claes <rubes.claes@gmail.com>
2018-09-24 13:49:35 +0200

    gitignore

--- skills/planet.js
+++ skills/planet.js
@@ -1,5 +1,4 @@
-var userDetails;
-
+var request = require('request');
 /**
  * Call to Nasa API: send message
  * 
@@ -27,29 +26,3 @@ exports.getPlanet = (text, context) => {
         })
     })
 }
-
-/**
- * Call to Conversation API: send message
- * 
- * @param {string} text 
- * @param {object} context 
- * @returns {promise}
- */
-exports.sendMessage = (text, context) => {
-    const payload = {
-      workspace_id: process.env.WORKSPACE_ID,
-      input: {
-        text: text
-      },
-      context: context
-    };
-    // Return new promise 
-    return new Promise((resolve, reject) => conversation.message(payload, function(err, data) {
-         // Do async job
-      if (err) {
-        reject(err);
-      } else {
-        resolve(data);
-      }
-    }));
-  };
